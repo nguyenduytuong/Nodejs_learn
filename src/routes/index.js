8420b1d9a5d7e1a8f6c1ebf258f73652f4e11456
ted <meoumap9@gmail.com>
2022-10-09 21:18:32 +0700

    insert courses

--- src/routes/index.js
+++ src/routes/index.js
@@ -18,6 +18,28 @@ function route(app) {
             .catch(next);
     })
 
+    app.get('/courses/create', (req, res, next) => {
+        res.render('courses/create');
+    });
+
+    app.post('/courses/store', (req, res, next) => {
+        res.json(req.body);
+        // const formData = req.body;
+        // const course = new Courses(formData);
+        // const save = course.save();
+        // // Courses.create(formData)
+        // if (save) {
+        //     res.send('save');
+        // } else {
+        //     res.send('no');
+        // }
+        // .then(course => {
+
+        // })
+        // .catch(next)
+    });
+
+
     app.get('/courses/:slug', (req, res, next) => {
         Courses.findOne({ id: req.params.id })
             .then(course => {
